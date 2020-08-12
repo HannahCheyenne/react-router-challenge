d3761471a22c915b2e528f1dd1cc0047a5346428
Hannah <HannahCheyenne@gmail.com>
2020-08-12 16:32:46 -0500

    completed mock interview

--- src/components/about-menu.js
+++ src/components/about-menu.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function AboutMenu(props) {
     return (
         <nav className="about-menu">
             <ul>
-                <li>Our mission</li>
-                <li>Our values</li>
-                <li>Our team</li>
-                <li>Our labradoodles</li>
+                <li><Link to="/about/mission">Our mission</Link></li>
+                <li><Link to="/about/values">Our values</Link></li>
+                <li><Link to="/about/team">Our team</Link></li>
+                <li><Link to="/about/labradoodles">Our labradoodles</Link></li>
             </ul>
         </nav>
     );
